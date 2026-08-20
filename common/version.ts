4ab0739b25a0572ee45c054d708f88fb7d440a4c
--global <73433905+onlygergo@users.noreply.github.com>
2026-08-20 19:38:45 +0100

    feat (frontend): polish frontend, add github source link

--- common/version.ts
+++ common/version.ts
@@ -1,6 +1,7 @@
 export const VERSION = 'v5.314';
 export const COMMIT = '18186a4';
 export const BUILD_DATE = 'Tue 21 Jul 15:53:56 BST 2026';
+export const SOURCE = 'https://github.com/GergoEB/MindustryStatsNode';
 
 export interface BuildInfo {
   commit: string;
